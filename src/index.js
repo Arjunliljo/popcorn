29348c5f6f7317b0824bfd1e766983071dd17fcc
Arjun cv <arjun7180@gmail.com>
2024-04-29 22:08:25 +0530

    Made ui responsive

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import "./index.css";
-// import App from "./App";
-import StarRating from "./StarRating";
+import "./index.css";
+import App from "./App";
+// import StarRating from "./StarRating";
 
-const maxLength = 10;
+//const maxLength = 10;
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <StarRating maxLength={maxLength} />
+    <App />
+    {/* <StarRating maxLength={maxLength} /> */}
   </React.StrictMode>
 );
