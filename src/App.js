24acf2acb4fc39c88518f566bf289daa69552027
Arjun cv <arjun7180@gmail.com>
2024-05-17 20:59:13 +0530

    s

--- src/App.js
+++ src/App.js
@@ -83,7 +83,7 @@ export default function App() {
     handleCloseMovie();
     fetching();
 
-    return () => controller.abort();
+    // return () => controller.abort();
   }, [query]);
 
   return (
